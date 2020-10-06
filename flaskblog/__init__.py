157e03c99578287d66af6f3e1f79173eb402fe66
malingzhi58 <ma_lingzhi@126.com>
2020-10-06 17:58:12 +0800

    1

---
+++ flaskblog/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+# 'login' means the def login, if the user does not log in, the page will jump to login
+login_manager.login_view = 'login'
+# 'info' the usage from bootstrap
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "mailaddress"
+app.config['MAIL_PASSWORD'] = "password"
+mail = Mail(app)
+
+
+from flaskblog import routes
